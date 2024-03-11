a37a6f9adfbbaa31cbc84298b9d86f64280b3051
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2024-03-11 17:48:11 +0300

    Putting in some basic CSS, working on the page layout.

--- js/draw.mjs
+++ js/draw.mjs
@@ -8,19 +8,23 @@ function drawPoint(x, y) {
 export default function draw(primitive) {
 	const canvas = document.querySelector("canvas")
 	const context = canvas.getContext("2d")
+	// ? Create separate Path2D's here, then do each one in a loop?
 	if (primitive instanceof NGon) {
 		const keys = Array.from(primitive.points.keys())
-		// ! THE 'fillStyle' is to be generalized!!!
-		context.beginPath()
+		// ! THE 'fillStyle' is to be generalized!!! [to a particular colour for each one point of a polygon..., and for each arrow]; 
 		for (const key of keys) {
-			context.fillStyle = "#00000"
-			drawPoint(...primitive.points[key])
+			// * For point...
+			context.strokeStyle = "#00000"	
+			context.beginPath()
+			drawPoint(...primitive.points[key])	
+			// * For polygon...
+			context.strokeStyle = "#00000"	
 			context.moveTo(...primitive.points[key])
 			if (primitive.connected[key])
-				context.lineTo(...primitive.points[(key + 1) % keys.length])
+				context.lineTo(...primitive.points[(key + 1) % keys.length])	
+			context.closePath()		
+			context.stroke()
 		}
-		context.closePath()	
-		context.stroke()
 	}
 }
 export function clear() {
