2519cabae38ca3fd89587589c9786ba7e8d1c756
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2024-04-09 00:52:22 +0300

    Alright. It didn't work again. This time doing it the ugly way [this does work]. Center fixed.

--- js/draw.mjs
+++ js/draw.mjs
@@ -1,3 +1,5 @@
+// ! Refactor [much]!!!
+
 import { black } from "./colors.mjs"
 import params from "./params.mjs"
 
@@ -13,31 +15,37 @@ function line(points, key) {
 
 // ^ idea: Add the appropriate 'casual' geometric functions/identities to future releases of 'math-expressions.js' (things like Pythagoreas, work with angles, trignonometry - this and other stuff...):
 function ellipse(points, elliptics, key) {
+	// ! Okay, the maths collapsed successfully. Apparently, self has forgotten that between two points only two different right-angle triangles can go [follows from that between two points only a single line exists];
 	const pair = [0, 1].map((i) => points[(key + i) % points.length])
 	const [x, y] = [0, 1].map((i) => pair.map((x) => x[i]))
 	const [dx, dy] = [x, y].map((p) => p[1] - p[0])
 
 	const preCenterAngle = elliptics[key][1] % 360
 
+	// ! CREATE DEFAULTS FOR THESE TWO! [would allow a very compact way of setting the thing...];
+	// ? Possibly, change places the 'color' argument in an ellipse and these two angles?
 	const [startAngle, endAngle] = elliptics[key].slice(2, 4)
 	const [isFirstLeft, isFirstAbove] = [dx >= 0, 0 < dy]
+
+	const isAlternate = preCenterAngle > 270
 
 	// ! Definition is flawed. Fix; [Requires more bloody branching AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH!]
 	// ! FIX THOSE! They are supposed to be incompatible;
 	// ! CLEAN UP ALL THAT MESS - refactor using '.map's + fix the ternary sequence for 'center';
 	// ? Should one, though? This is good for semantics. Makes code far more intuitive...
 	// * How about the following - if this works, then (initially) oneself commits as-is, then cleans up.
-	const isCenterBelow = Math.abs(dx) > Math.abs(dy) && preCenterAngle < 90
-	const isCenterAbove = Math.abs(dx) > Math.abs(dy) && preCenterAngle > 270
-	const isCenterRight = Math.abs(dy) > Math.abs(dx) && preCenterAngle < 90
-	const isCenterLeft = Math.abs(dy) > Math.abs(dx) && preCenterAngle > 270
+	const isCenterBelow = Math.abs(dx) > Math.abs(dy) && !isAlternate
+	const isCenterAbove = Math.abs(dx) > Math.abs(dy) && isAlternate
+	const isCenterRight = Math.abs(dy) > Math.abs(dx) && !isAlternate
+	const isCenterLeft = Math.abs(dy) > Math.abs(dx) && isAlternate
 
-	const centerAngle = (isCenterAbove ? (x) => x - 270 : (x) => x)(preCenterAngle)
+	const centerAngle = (isAlternate ? (x) => x - 270 : (x) => x)(preCenterAngle)
 
 	const diagLen = Math.sqrt(dx ** 2 + dy ** 2)
-	const radius = ["cos", "sin"]
+	const _radius = ["cos", "sin"]
 		.map((x) => Math[x])
 		.map((f) => diagLen * f(toRadians(centerAngle)))
+	const radius = _radius[0]
 	// ! leave alone for now... Center's still fucked.
 	const rotationAngle = 0 /* toRadians(90 - centerAngle) + Math.acos(dx / diagLen) */
 
@@ -55,28 +63,105 @@ function ellipse(points, elliptics, key) {
 	console.log("first above?")
 	console.log(isFirstAbove)
 
-	const center = [
-		[x, "cos"],
-		[y, "sin"]
-	].map(([z, f], i) => {
-		return (
-			z[+(isCenterAbove || isCenterBelow ? !isFirstLeft : !isFirstAbove)] +
-			(-1) ** (isCenterAbove ? i : isCenterRight ? 0 : isCenterBelow ? 0 : !i) *
-				radius[+(isCenterAbove || isCenterBelow ? !isFirstLeft : !isFirstAbove)] *
-				Math[f](
-					toRadians((isFirstAbove ? (x) => x : (x) => 90 - x)(centerAngle)) +
-						Math.acos(Math.abs(dx / diagLen))
-				)
-		)
-	})
+	const term = [
+		toRadians(90) - (toRadians(centerAngle) - Math.acos(dx / diagLen)),
+		Math.acos(dy / diagLen) - toRadians(centerAngle),
+		toRadians(90) - (toRadians(centerAngle) - Math.acos(dy / diagLen)),
+		Math.acos(dx / diagLen) - toRadians(centerAngle)
+	]
+	const first = pair[0]
+
+	const fs = ["cos", "sin"]
+
+	// const _center = isCenterAbove
+	// 	? isFirstLeft && isCenterAbove
+	// 		? first.map((x, i) => x + (-1) ** i * radius * Math[fs[+!i]](term[0]))
+	// 		: isFirstLeft
+	// 		? first.map((x, i) => x + (-1) ** i * radius * Math[fs[+!i]](term[1]))
+	// 		: isFirstAbove
+	// 		? first.map((x, i) => x + (-1) ** 1 * radius * Math[fs[+!i]](term[0]))
+	// 		: first.map((x, i) => x + (-1) ** 1 * radius * Math[fs[+!i]](term[2]))
+	// 	: isCenterBelow ? 
+	// 		isFirstAbove && isFirstLeft ? 
+	// 		: 
+	// 	: []
+
+	// ! inomplete - fix;
+	// ! 16 distinct cases - horrid. make look good [refactor conditionals into a single conditionaless expression with bool-Number arithmetic...]
+	const center = isCenterAbove
+		? isFirstLeft && isFirstAbove
+			? [
+					first[0] + radius * Math.sin(term[0]),
+					first[1] - radius * Math.cos(term[0])
+			  ]
+			: isFirstLeft
+			? [
+					first[0] + radius * Math.sin(term[1]),
+					first[1] - radius * Math.cos(term[1])
+			  ]
+			: isFirstAbove
+			? [
+					first[0] - radius * Math.sin(term[0]),
+					first[1] - radius * Math.cos(term[0])
+			  ]
+			: [
+					first[0] - radius * Math.sin(term[2]),
+					first[1] - radius * Math.cos(term[2])
+			  ]
+		: isCenterBelow
+		? isFirstLeft && isFirstAbove
+			? [
+					first[0] + radius * Math.sin(term[1]),
+					first[1] + radius * Math.cos(term[1])
+			  ]
+			: isFirstLeft
+			? [
+					first[0] + radius * Math.sin(term[0]),
+					first[1] + radius * Math.cos(term[0])
+			  ]
+			: isFirstAbove
+			? [
+					first[0] - radius * Math.sin(term[1]),
+					first[1] + radius * Math.cos(term[1])
+			  ]
+			: [
+					first[0] - radius * Math.sin(term[0]),
+					first[1] + radius * Math.cos(term[0])
+			  ]
+		: isCenterRight
+		? isFirstLeft && isFirstAbove
+			? [
+					first[0] + radius * Math.cos(term[3]),
+					first[1] + radius * Math.sin(term[3])
+			  ]
+			: isFirstLeft
+			? [
+					first[0] + radius * Math.cos(term[3]),
+					first[1] - radius * Math.sin(term[3])
+			  ]
+			: isFirstAbove
+			? [
+					first[0] + radius * Math.cos(term[2]),
+					first[1] + radius * Math.sin(term[2])
+			  ]
+			: [
+					first[0] + radius * Math.cos(term[2]),
+					first[1] - radius * Math.sin(term[2])
+			  ]
+		: isFirstLeft && isFirstAbove
+		? [first[0] - radius * Math.cos(term[2]), first[1] + radius * Math.sin(term[2])]
+		: isFirstLeft
+		? [first[0] - radius * Math.cos(term[2]), first[1] - radius * Math.sin(term[2])]
+		: isFirstAbove
+		? [first[0] - radius * Math.cos(term[1]), first[1] + radius * Math.sin(term[1])]
+		: [first[0] - radius * Math.cos(term[3]), first[1] - radius * Math.cos(term[3])]
 
 	// ? Is order (indexation) of 'radius' correct (and general) here? Check...
 	context.ellipse(
 		...center,
-		...radius,
+		..._radius,
 		rotationAngle,
-		toRadians(startAngle),
-		toRadians(endAngle)
+		...[startAngle, endAngle].map(toRadians)
 	)
 
 	// ! DEBUG CODE...
